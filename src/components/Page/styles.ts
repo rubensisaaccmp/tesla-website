6e0ff09e26c75997411bb06f34fdf81d37a090ba
Rubens Isaac <rubensisaaccmp@gmail.com>
2020-10-15 10:19:39 -0300

    first commit

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+.colored:nth-child(1) {
+background-image: url("https://c.wallhere.com/photos/3b/46/1366x768_px_Blue_Cars_car_Nissan_Nissan_Skyline_GT_R-745481.jpg!d");
+}
+.colored:nth-child(2) {
+background-image: url("https://www.desktopbackground.org/download/1366x768/2010/12/13/125831_dodge-challenger-tuning-free-car-wallpapers-hd-dodge-challenger_1920x1080_h.jpg") ;
+}
+.colored:nth-child(3) {
+background-image: url("https://www.osmais.com/wallpapers/201603/carro-no-deserto-wallpaper-1366x768.jpg");
+}
+.colored:nth-child(4) {
+background-image: url("https://wallpaperaccess.com/full/498185.jpg");
+}
+.colored:nth-child(5) {
+background-image: url("https://images7.alphacoders.com/495/thumb-1920-495187.jpg");
+}
+.colored:nth-child(6) {
+background-image: url("https://s1.1zoom.me/b5050/786/400603-svetik_1366x768.jpg");
+}
+.colored:nth-child(7) {
+background-image: url("https://hdqwalls.com/download/volvo-truck-and-car-hd-1366x768.jpg");
+}
+`;
+
+export const Spacer = styled.div`
+    height: 15vh;
+    background: #77e68c;
+`
